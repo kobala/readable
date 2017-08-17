e5328e8f706dac2998b973ca94a973332465d3d8
Irakli Kobalia <qobalia92@gmail.com>
2017-08-17 19:50:12 +0400

    fix: fix wranings for emojies and targets in footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Footer = () => (
     <div id="footer">
         <div className="container">
-            <p className="text-muted credit">Made with 💚 &nbsp;by <a href="https://github.com/kobala" target="_blank">Kobala </a>
-                for <a href="https://www.udacity.com/course/react-nanodegree--nd019">Udacity's React Nanodegree course</a>
+            <p className="text-muted credit">Made with <span role="img" aria-label="Heart">💚</span>
+                &nbsp;by <a href="https://github.com/kobala" target="_blank" rel="noopener noreferrer">Kobala </a> for
+                <a href="https://www.udacity.com/course/react-nanodegree--nd019" target="_blank" rel="noopener noreferrer">
+                    Udacity's React Nanodegree course
+                </a>
             </p>
         </div>
     </div>
