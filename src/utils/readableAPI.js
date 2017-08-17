077459e16780ee0459a2fdec5f22d409d3f5fcae
Irakli Kobalia <qobalia92@gmail.com>
2017-08-17 17:11:47 +0400

    feat: Add readableAPI util

---
+++ src/utils/readableAPI.js
@@ -0,0 +1,108 @@
+const api = "http://localhost:5001/"
+
+let token = localStorage.token
+
+if (!token)
+    token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+    'Accept': 'application/json',
+    'Authorization': token
+}
+
+export const getCategories = () =>
+    fetch(`${api}/categories`, { headers })
+        .then(res => res.json())
+        .then(data => data.categories)
+
+export const getPostsByCategory = (category) =>
+    fetch(`${api}/${category}/posts`, { headers })
+        .then(res => res.json())
+
+export const getPosts = () =>
+    fetch(`${api}/posts`, { headers })
+        .then(res => res.json())
+
+export const addPost = (post) =>
+    fetch(`${api}/posts`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    })
+
+export const getPostById = (postId) =>
+    fetch(`${api}/posts/${postId}`, { headers })
+        .then(res => res.json())
+
+export const votePost = (postId, option) =>
+    fetch(`${api}/posts/${postId}`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ option })
+    }).then(res => res.json())
+
+export const editPost = (postId, postDetails) =>
+    fetch(`${api}/posts/${postId}`, {
+        method: 'PUT',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postDetails)
+    })
+
+export const deletePost = (postId) =>
+    fetch(`${api}/posts/${postId}`, {
+        method: 'DELETE',
+        headers: headers
+    })
+
+export const getCommentsByPostId = (postId) =>
+    fetch(`${api}/posts/${postId}/comments`, { headers })
+        .then(res => res.json())
+
+export const addComment = (comment) =>
+    fetch(`${api}/comments`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    })
+
+export const getCommentById = (commentID) =>
+    fetch(`${api}/comments/${commentID}`, { headers })
+        .then(res => res.json())
+
+export const voteComment = (commentId, option) =>
+    fetch(`${api}/comments/${commentId}`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ option })
+    }).then(res => res.json())
+
+export const editComment = (commentId, commentDetails) =>
+    fetch(`${api}/comments/${commentId}`, {
+        method: 'PUT',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(commentDetails)
+    })
+
+export const deleteComment = (commentId) =>
+    fetch(`${api}/comments/${commentId}`, {
+        method: 'DELETE',
+        headers: headers
+    })
